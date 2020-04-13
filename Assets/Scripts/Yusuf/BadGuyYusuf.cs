d754932e014e84081e046b2123d052a886dcdf1d
Yusuf-Ali-1 <62290174+Yusuf-Ali-1@users.noreply.github.com>
2020-04-13 04:55:24 -0400

    BadGuyYusuf
    Finished movement and position of enemy included hit function

--- Assets/Scripts/Yusuf/BadGuyYusuf.cs
+++ Assets/Scripts/Yusuf/BadGuyYusuf.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class BadGuyYusuf : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+
+    //Crated two variables
+
+    public int EnemySpeed;
+
+    public int XMoveDirection;
+
 
     // Update is called once per frame
     void Update()
     {
-        
+        //Changes direction when enemy hits Player
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, new Vector2(XMoveDirection, 0));
+
+        //Defines enemy speed and position
+        gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(XMoveDirection, 0) * EnemySpeed;
+
+        //Define hit 
+        if (hit.distance < 0.7f) {
+            Flip();
+        }
+
+      }
+    
+    void Flip() {
     }
 }
