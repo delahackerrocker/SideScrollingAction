173594447757627d8b5336dd6445e2e8a65fc9fe
Dela <stevedelatorre@gmail.com>
2020-04-13 09:10:58 -0400

    Played With Yusufs stuff

--- Assets/Scripts/Yusuf/BadGuyYusuf.cs
+++ Assets/Scripts/Yusuf/BadGuyYusuf.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class BadGuyYusuf : MonoBehaviour
 {
-
+    public GameObject player;
     //Crated two variables
 
     public int EnemySpeed;
@@ -18,12 +18,18 @@ public class BadGuyYusuf : MonoBehaviour
         //Changes direction when enemy hits Player
         RaycastHit2D hit = Physics2D.Raycast(transform.position, new Vector3(XMoveDirection, 0));
 
+        if ((player.transform.position.x - this.transform.position.x) < 2f ) 
+        {
+            // flip around
+            Flip();
+        }
+
         //Defines enemy speed and position
         gameObject.GetComponent<Rigidbody2D>().velocity = new Vector3(XMoveDirection, 0) * EnemySpeed;
 
         //Define hit 
         if (hit.distance < 0.7f) {
-            Flip();
+            //Flip();
         }
 
 
